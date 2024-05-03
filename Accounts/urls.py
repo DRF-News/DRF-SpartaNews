d5dfa813ee1e041b88db1459b1e2730b93a4903a
HyunHyoMin <159979121+HyunHyoMin@users.noreply.github.com>
2024-05-03 14:49:49 +0900

    Account email-Unique 오류 디버깅

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from . import views
 
 urlpatterns = [
     path('register/', views.register),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token_refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('<int:user_id>/', views.AccountMangement.as_view()),
 ]
