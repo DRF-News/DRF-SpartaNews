ffb2f63a1f9d221c0a984001ae7732a369b699bb
HyunHyoMin <159979121+HyunHyoMin@users.noreply.github.com>
2024-05-03 14:53:21 +0900

    login, logout 구현

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenBlacklistView,
 )
 from . import views
 
 urlpatterns = [
     path('register/', views.register),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path('token_refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('<int:user_id>/', views.AccountMangement.as_view()),
 ]
