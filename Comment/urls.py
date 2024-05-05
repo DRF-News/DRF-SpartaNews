1601eceef50c78321e1c6a16dbdda66a2e995a4f
leejinwon012 <leejinwon012@naver.com>
2024-05-06 06:09:47 +0900

    Comment 조회하기 구현 완료

--- Comment/urls.py
+++ Comment/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CommentListCreateAPIView
+from .views import CommentListCreateAPIView, CommentRetrieveAPIView
 
 urlpatterns = [
     path('<int:post_id>/', CommentListCreateAPIView.as_view(), name='comment_list_create'),
+    path('<int:post_id>/', CommentRetrieveAPIView.as_view(), name='comment_retrieve'),
 ]
--- Comment/views.py
+++ Comment/views.py
@@ -7,6 +7,7 @@ from .models import Comment
 from .serializers import CommentSerializer
 
 
+# Comment 작성하기 / 작성 후 해당 게시글에 달린 댓글들 전부 조회회
 class CommentListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
 
@@ -22,3 +23,12 @@ class CommentListCreateAPIView(generics.ListCreateAPIView):
             queryset = self.get_queryset()
             return Response(self.serializer_class(queryset, many=True).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Comment 보여주기 / 해당 게시물에 대한 모든 Comment 조회 가능
+class CommentRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        return post.comments.all()
