d0381e9e8f698a9c9e603096c8ffdb2e3c982593
kngslbm <kangseulbeom@gmail.com>
2024-05-08 15:21:34 +0900

    default pagenation setting

--- DRF_SpartarNews/settings.py
+++ DRF_SpartarNews/settings.py
@@ -111,8 +111,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
+    ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 SIMPLE_JWT = {
