33324b5bec190995428d1c1ff7bcf685df27ca5a
kngslbm <kangseulbeom@gmail.com>
2024-05-08 17:52:40 +0900

    Search (1.0 ver)
    단순히 keyword가 포함된 Post 조회 가능함.

--- Search/views.py
+++ Search/views.py
@@ -1,8 +1,29 @@
 from rest_framework import generics
+from django.db.models import Q
 from Post.models import Post
 from .serializers import PostSerializer
 
 
+# class PostListAPIView(generics.ListAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
 class PostListAPIView(generics.ListAPIView):
-    queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get_queryset(self):
+        query_params = self.request.query_params
+        title = query_params.get("title")
+        content = query_params.get("content")
+        username = query_params.get("username")
+
+        q = Q()
+        if title:
+            q &= Q(title__icontains=title)
+        if content:
+            q &= Q(content__icontains=content)
+        if username:
+            q &= Q(user__username__icontains=username)
+
+        return Post.objects.filter(q)
